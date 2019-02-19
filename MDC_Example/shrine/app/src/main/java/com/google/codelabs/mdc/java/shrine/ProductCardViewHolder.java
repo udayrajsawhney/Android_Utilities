9afab87d5199ec4bba7065e99cc175854d048ea0
udayrajsawhney <udayraj.s16@iiits.in>
2019-02-20 00:21:53 +0530

    added MDC example

---
+++ MDC_Example/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductCardViewHolder.java
@@ -0,0 +1,13 @@
+package com.google.codelabs.mdc.java.shrine;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.View;
+
+public class ProductCardViewHolder extends RecyclerView.ViewHolder {
+
+    public ProductCardViewHolder(@NonNull View itemView) {
+        super(itemView);
+        //TODO: Find and store views from itemView
+    }
+}
---
+++ MDC_Example/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductGridFragment.java
@@ -0,0 +1,18 @@
+package com.google.codelabs.mdc.java.shrine;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ProductGridFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.shr_product_grid_fragment, container, false);
+    }
+}
