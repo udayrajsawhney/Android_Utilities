a5f0e0b769e0c33eee6a0868b4d9bad4f46703d3
udayrajsawhney <udayraj.s16@iiits.in>
2019-02-25 01:14:21 +0530

    added backdrop example

---
+++ BackdropExample/app/src/main/java/com/simplyillustrate/backdropexample/ListFragment.java
@@ -0,0 +1,40 @@
+package com.simplyillustrate.backdropexample;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+public class ListFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.list_fragment, container, false);
+        setUpToolbar(view);
+        return view;
+    }
+
+    private void setUpToolbar(View view) {
+        Toolbar toolbar = view.findViewById(R.id.app_bar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        if (activity != null) {
+            activity.setSupportActionBar(toolbar);
+        }
+
+        toolbar.setNavigationOnClickListener(new NavigationIconClickListener(
+                getContext(),
+                view.findViewById(R.id.product_grid),
+                new AccelerateDecelerateInterpolator(),
+                ContextCompat.getDrawable(getActivity(), R.drawable.app_branded_menu),
+                ContextCompat.getDrawable(getActivity(), R.drawable.app_close_menu)));
+    }
+}
---
+++ BackdropExample/app/src/main/java/com/simplyillustrate/backdropexample/MainActivity.java
@@ -0,0 +1,35 @@
+package com.simplyillustrate.backdropexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements NavigationHost {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        navigateToFragment();
+    }
+
+    public void navigateToFragment() {
+        ((NavigationHost) (MainActivity.this)).navigateTo(new ListFragment(), false);
+    }
+
+    @Override
+    public void navigateTo(Fragment fragment, boolean addToBackstack) {
+        FragmentTransaction transaction =
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.container, fragment);
+
+        if (addToBackstack) {
+            transaction.addToBackStack(null);
+        }
+
+        transaction.commit();
+    }
+}
