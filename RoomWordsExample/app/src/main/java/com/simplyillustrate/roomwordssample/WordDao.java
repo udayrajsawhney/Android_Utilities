0d276956474eaf5d92ef2ef8eecb597c3902e007
udayrajsawhney <udayraj.s16@iiits.in>
2019-03-07 22:21:39 +0530

    added Room database example

---
+++ RoomWordsExample/app/src/main/java/com/simplyillustrate/roomwordssample/WordDao.java
@@ -0,0 +1,22 @@
+package com.simplyillustrate.roomwordssample;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+@Dao
+public interface WordDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAllWords();
+}
---
+++ RoomWordsExample/app/src/main/java/com/simplyillustrate/roomwordssample/WordViewModel.java
@@ -0,0 +1,29 @@
+package com.simplyillustrate.roomwordssample;
+
+import android.app.Application;
+
+import java.util.List;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+public class WordViewModel extends AndroidViewModel {
+
+    private WordRepository mRepository;
+
+    private LiveData<List<Word>> mAllWords;
+
+    public WordViewModel(Application application) {
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+    public void insert(Word word) {
+        mRepository.insert(word);
+    }
+}
